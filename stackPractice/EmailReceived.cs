0b8632be68f0df3d11bf397c8fd4c027a1df13a6
En Emerson <007emersone@gmail.com>
2018-05-25 00:03:21 -0500

    First commit

---
+++ stackPractice/EmailReceived.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stackPractice
+{
+    enum DayOfWeek
+    {
+        Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
+    }
+    class EmailReceived
+    {
+        internal string from;
+        internal string content;
+        internal DayOfWeek dayReceived;
+
+        public EmailReceived()
+        {
+            //empty default constructor
+        }
+        public EmailReceived(string pName, string pContent, DayOfWeek pDayReceived)
+        {
+            from = pName;
+            content = pContent;
+            dayReceived = pDayReceived;
+        }
+    }
+}
---
+++ stackPractice/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace stackPractice
+{
+    public partial class Form1 : Form
+    {
+        Stack<EmailReceived> myEmail = new Stack<EmailReceived>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //we are assuming "today" is saturday
+
+            myEmail.Push(new EmailReceived("John", 
+                "Let's go for a walk next week on Monday.", DayOfWeek.Sunday));
+            myEmail.Push(new EmailReceived("Mary", 
+                "I talked to John about going for a walk next week on Monday.", DayOfWeek.Monday));
+            myEmail.Push(new EmailReceived(
+                "Smith", "Anna and I will be going to a movie next Sunday.", DayOfWeek.Tuesday));
+            myEmail.Push(new EmailReceived(
+                "Anna", "Smith and I will be going to see a movie on Sunday", DayOfWeek.Wednesday));
+            myEmail.Push(new EmailReceived(
+                "John", "I can no longer make it to hangout guys....", DayOfWeek.Thursday));
+            myEmail.Push(new EmailReceived(
+                "Mary", "We need to meet tomorrow.", DayOfWeek.Friday));
+
+            do
+            {
+                EmailReceived anyEmail = (EmailReceived)myEmail.Pop();
+
+                MessageBox.Show(anyEmail.from + ": " + anyEmail.content + "\nSent: " + anyEmail.dayReceived.ToString());
+            }
+            while (myEmail.Count != 0);
+        }
+    }
+}
